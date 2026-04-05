4724bee57a21f849eef886202a88aaa5862d8fa4
IshanTaunk <ishan.taunk444@gmail.com>
2026-04-05 19:51:26 +0530

    fix: minor fix

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -1,4 +1,5 @@
 import { useProducts } from "../features/products/hooks/useProducts";
+import SearchBar from "../features/searchBar/searchBar";
 
 const Home = () => {
   const { data, isLoading, error} = useProducts();
@@ -6,37 +7,41 @@ const Home = () => {
   if(isLoading) return <div>Loading...</div>;
   if(error) return <div> Failed to load</div>;
 
-  return (
-<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 p-4">
-  {data?.map((product) => {
-    return (
-      <div
-        key={`product.id`}
-        className="w-[300px] bg-white rounded-2xl shadow-sm hover:shadow-md transition p-3"
-      >
-        {/* Image container (square) */}
-        <div className="w-full aspect-square overflow-hidden rounded-xl bg-gray-100">
-          <img
-            src={product.image.url}
-            alt={product.image.description}
-            className="w-full h-full object-cover"
-          />
-        </div>
+  return (<>
+  <div>
+    <SearchBar/>
+  </div>
+  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 p-4">
+    {data?.map((product) => {
+      return (
+        <div
+          key={`product.id`}
+          className="w-[300px] bg-white rounded-2xl shadow-sm hover:shadow-md transition p-3"
+        >
+          {/* Image container (square) */}
+          <div className="w-full aspect-square overflow-hidden rounded-xl bg-gray-100">
+            <img
+              src={product.image.url}
+              alt={product.image.description}
+              className="w-full h-full object-cover"
+            />
+          </div>
 
-        {/* Product info */}
-        <div className="mt-3 space-y-1">
-          <h3 className="text-sm font-medium text-gray-800 line-clamp-2">
-            {product.name}
-          </h3>
+          {/* Product info */}
+          <div className="mt-3 space-y-1">
+            <h3 className="text-sm font-medium text-gray-800 line-clamp-2">
+              {product.name}
+            </h3>
 
-          <div className="text-lg font-semibold text-green-600">
-            ₹{product.price}
+            <div className="text-lg font-semibold text-green-600">
+              ₹{product.price}
+            </div>
           </div>
         </div>
-      </div>
-    );
-  })}
-</div>
+      );
+    })}
+  </div>
+</>
   )
 }
 
